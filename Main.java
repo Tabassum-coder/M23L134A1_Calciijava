042a80a727efbc8281aaa57e70c258a98d7583b7
Tabassum-code <tabuj2706@gmail.com>
2024-10-08 23:44:37 +0530

    Calcii

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        int diff=b-a;
+        int prod=a*b;
+        int quotient=b/a;
+        System.out.println("Sum of a "+a+" and "+b+ " is "+sum);
+        System.out.println("Difference of a " + a + " and " + b + " is " + diff);
+        System.out.println("Product of a " + a + " and " + b + " is " + prod);
+        System.out.println("Quotient  of a " + a + " and " + b + " is " + quotient);
+    }
+}
